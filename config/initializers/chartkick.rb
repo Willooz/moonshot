977bb1da380fc825ab4a50e7b28410987f4cdd3b
Willem <willem.browne@gmail.com>
2015-05-06 19:41:48 +0200

    i cant stand css anymore

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,5 +1,5 @@
 Chartkick.options = {
   height: "400px",
-  colors: ["#714A95", "#DBBDE9"],
+  colors: ["#D6D2E5", "#352A5E"],
   html: '<div id="%{id}" style="height: %{height};" class="graphy-thingy">Loading...</div>'
 }
