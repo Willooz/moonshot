8293291ce0af62ab350f16f7077895d92b2dc789
marina starkova <marina.starkova@gmail.com>
2015-05-07 15:28:53 +0200

    favicon

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+
+
+
 Account.create(name: "Avengers")
 Account.create(name: "X-Men")
 
