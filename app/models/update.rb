bef3d5c3d97ba6128ead2eb5a784d32a58a61722
Nicolas Feer <atmosfeer@gmail.com>
2015-05-07 17:39:03 +0200

    Fixed Update Moon

--- app/models/update.rb
+++ app/models/update.rb
@@ -3,4 +3,16 @@ class Update < ActiveRecord::Base
   belongs_to :shot
   has_many :events, as: :trigger
 
+  def percentage_done
+    bl = self.current_value
+    tg = self.shot.target_value
+    if bl < tg
+      diff = tg - self.shot.baseline_value
+      (((bl.to_f - self.shot.baseline_value) / diff) * 100).round
+    else
+      diff = self.baseline_value - tg
+      (((self.shot.baseline_value - bl) / diff) * 100).round
+    end
+  end
+
 end
